08b4f248f3765ca3447edf817456a813a3d64a11
Mehedi Hasan Khan <107515804+MHKhanCou@users.noreply.github.com>
2025-09-17 01:07:47 +0600

    Create problemK__Dreaming_of_National_IUPC.cpp

---
+++ problemK__Dreaming_of_National_IUPC.cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/gym/106057/problem/K
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    cout << "We want national level IUPC in CoU\n";
+
+    return 0;
+}
